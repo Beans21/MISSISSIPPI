bdb5c6bb83ea6c01ac01a295ea80f9d502315082
Beans21 <SKardashian19@windwardschool.org>
2016-09-07 15:58:03 -0700

    Homework- MISSISSIPPI

---
+++ src/writingMISSISSIPPI.java
@@ -0,0 +1,57 @@
+
+public class writingMISSISSIPPI {
+
+	public static void main(String[] args) {
+		mLetter();
+		iLetter();
+		sLetter();
+		sLetter();
+		iLetter();
+		sLetter();
+		sLetter();
+		iLetter();
+		pLetter();
+		pLetter();
+		iLetter();
+	}
+	
+	public static void mLetter() {
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M\n");
+	}
+	
+	public static void iLetter() {
+		System.out.println("IIIII");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("IIIII\n");
+	}
+	
+	public static void sLetter() {
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS\n");
+	}
+	
+	public static void pLetter() {
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P\n");
+	}
+}
